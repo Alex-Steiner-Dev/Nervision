17ca898f11cb1d47895a260b1b3e4cb172425105
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-30 17:31:21 +0100

    HTTP Request to server

--- Server/server.py
+++ Server/server.py
@@ -8,4 +8,4 @@ def index():
     return getImage("desert with cacti")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=1000)
--- Unity/Assets/Scripts/ImageGeneration.cs
+++ Unity/Assets/Scripts/ImageGeneration.cs
@@ -1,12 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class ImageGeneration : MonoBehaviour
 {
     [SerializeField] private string serverUrl;
 
-    private void generateImage() {
+    private void Start()
+    {
+        generateImage("test");
+    }
 
+    private void generateImage(string prompt)
+    {
+        using (var wb = new WebClient())
+        {
+            var response = wb.DownloadString(serverUrl);
+            print(response);
+        }
     }
 }
