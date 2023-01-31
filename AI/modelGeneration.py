acdb685d0a7f2375bd173ef37afe74b9ea726f1d
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-31 08:15:11 +0100

    Basic 3D Model Generation

--- AI/modelGeneration.py
+++ AI/modelGeneration.py
@@ -1,37 +1,63 @@
 import numpy as np
 from stl import mesh
+from PIL import Image
 
-# Define the 8 vertices of the cube
-vertices = np.array([\
-    [-1, -1, -1],
-    [+1, -1, -1],
-    [+1, +1, -1],
-    [-1, +1, -1],
-    [-1, -1, +1],
-    [+1, -1, +1],
-    [+1, +1, +1],
-    [-1, +1, +1]])
-# Define the 12 triangles composing the cube
-faces = np.array([\
-    [0,3,1],
-    [1,3,2],
-    [0,4,7],
-    [0,7,3],
-    [4,5,6],
-    [4,6,7],
-    [5,1,2],
-    [5,2,6],
-    [2,3,6],
-    [3,7,6],
-    [0,1,5],
-    [0,5,4]])
+grey_img = Image.open("desert.jpg").convert('L')
 
+
+max_size=(500,500)
+max_height=10
+min_height=0
+
+#height=0 for minPix
+#height=maxHeight for maxPIx
+
+grey_img.thumbnail(max_size)
+imageNp = np.array(grey_img)
+maxPix=imageNp.max()
+minPix=imageNp.min()
+
+
+
+print(imageNp)
+(ncols,nrows)=grey_img.size
+
+vertices=np.zeros((nrows,ncols,3))
+
+for x in range(0, ncols):
+  for y in range(0, nrows):
+    pixelIntensity = imageNp[y][x]
+    z = (pixelIntensity * max_height) / maxPix
+    #print(imageNp[y][x])
+    vertices[y][x]=(x, y, z)
+
+faces=[]
+
+for x in range(0, ncols - 1):
+  for y in range(0, nrows - 1):
+    # create face 1
+    vertice1 = vertices[y][x]
+    vertice2 = vertices[y+1][x]
+    vertice3 = vertices[y+1][x+1]
+    face1 = np.array([vertice1,vertice2,vertice3])
+
+    # create face 2 
+    vertice1 = vertices[y][x]
+    vertice2 = vertices[y][x+1]
+    vertice3 = vertices[y+1][x+1]
+
+    face2 = np.array([vertice1,vertice2,vertice3])
+
+    faces.append(face1)
+    faces.append(face2)
+
+print(f"number of faces: {len(faces)}")
+facesNp = np.array(faces)
 # Create the mesh
-cube = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+surface = mesh.Mesh(np.zeros(facesNp.shape[0], dtype=mesh.Mesh.dtype))
 for i, f in enumerate(faces):
     for j in range(3):
-        print(vertices[f[j],:])
-        cube.vectors[i][j] = vertices[f[j]]
-
+        surface.vectors[i][j] = facesNp[i][j]
 # Write the mesh to file "cube.stl"
-cube.save('cube.stl')
+surface.save('surface.stl')
+print(surface)
