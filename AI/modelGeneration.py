f0971b0c37d783d3c75be5634093a8edd8f6be13
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-31 09:09:33 +0100

    Cloud Point

--- AI/modelGeneration.py
+++ AI/modelGeneration.py
@@ -2,24 +2,18 @@ import numpy as np
 from stl import mesh
 from PIL import Image
 
-grey_img = Image.open("desert.jpg").convert('L')
+grey_img = Image.open("test.jpeg").convert('L')
 
 
 max_size=(500,500)
-max_height=10
+max_height=100
 min_height=0
-
-#height=0 for minPix
-#height=maxHeight for maxPIx
 
 grey_img.thumbnail(max_size)
 imageNp = np.array(grey_img)
 maxPix=imageNp.max()
 minPix=imageNp.min()
 
-
-
-print(imageNp)
 (ncols,nrows)=grey_img.size
 
 vertices=np.zeros((nrows,ncols,3))
@@ -28,20 +22,17 @@ for x in range(0, ncols):
   for y in range(0, nrows):
     pixelIntensity = imageNp[y][x]
     z = (pixelIntensity * max_height) / maxPix
-    #print(imageNp[y][x])
     vertices[y][x]=(x, y, z)
 
 faces=[]
 
 for x in range(0, ncols - 1):
   for y in range(0, nrows - 1):
-    # create face 1
     vertice1 = vertices[y][x]
     vertice2 = vertices[y+1][x]
     vertice3 = vertices[y+1][x+1]
     face1 = np.array([vertice1,vertice2,vertice3])
 
-    # create face 2 
     vertice1 = vertices[y][x]
     vertice2 = vertices[y][x+1]
     vertice3 = vertices[y+1][x+1]
@@ -51,13 +42,11 @@ for x in range(0, ncols - 1):
     faces.append(face1)
     faces.append(face2)
 
-print(f"number of faces: {len(faces)}")
 facesNp = np.array(faces)
-# Create the mesh
+
 surface = mesh.Mesh(np.zeros(facesNp.shape[0], dtype=mesh.Mesh.dtype))
 for i, f in enumerate(faces):
     for j in range(3):
         surface.vectors[i][j] = facesNp[i][j]
-# Write the mesh to file "cube.stl"
+
 surface.save('surface.stl')
-print(surface)
