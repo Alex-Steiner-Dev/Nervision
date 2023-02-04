ba924b87b98c64739c5c9af88a3da6a6217585df
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-04 13:36:37 +0100

    Point Cloud

--- AI/Utils/depth_to_point_cloud.py
+++ AI/Utils/depth_to_point_cloud.py
@@ -1,11 +1,12 @@
 import open3d as o3d
 import numpy as np
 import matplotlib.pyplot as plt
+import trimesh
 import os
 import sys
 import cv2
 
-color_raw = o3d.io.read_image("rgb.png")
+color_raw = o3d.io.read_image("rgb.jpg")
 depth_raw = o3d.io.read_image("depth.png")
 rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color_raw, depth_raw)
 
@@ -14,4 +15,9 @@ camera_intrinsic = o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraInt
 pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, camera_intrinsic)
     
 pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-o3d.visualization.draw_geometries([pcd])
+
+radii = [0.005, 0.01, 0.02, 0.04]
+rec_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(
+    pcd, o3d.utility.DoubleVector(radii))
+o3d.visualization.draw_geometries([pcd, rec_mesh])
+
