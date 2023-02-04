9f068a394e250ed10956dfbd7bfb4de326f86450
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-04 16:43:22 +0100

    Loading Bar

--- AI/Train/train.py
+++ AI/Train/train.py
@@ -3,23 +3,34 @@ import os
 import sys
 from mesh_to_point_cloud import *
 
+from tqdm import tqdm
+from time import sleep
+
 modelName = "model.ape"
 subDir = glob.glob("../Data/*")
 
-open(modelName, "w").close()
-file = open(modelName, "a")
+def train():
+    bar = tqdm(range(0, len(subDir)), desc = "Training Model")            
 
-for i in subDir:
-    three_d_model = glob.glob(i + "/*.obj")
-    description = glob.glob(i + "/*.txt")
-    
-    mesh_to_point_cloud(three_d_model[0], i)
-    point_cloud = glob.glob(i + "/*.pc")
+    open(modelName, "w").close()
+    file = open(modelName, "a")
 
-    file.write("# " + os.path.basename(three_d_model[0]) + "\n")
+    for i in subDir:            
+        three_d_model = glob.glob(i + "/*.obj")
+        description = glob.glob(i + "/*.txt")
+            
+        mesh_to_point_cloud(three_d_model[0], i)
+        point_cloud = glob.glob(i + "/*.pc")
 
-    with open(description[0]) as description_file:
-        file.write("// " + description_file.read() + "\n")
+        file.write("# " + os.path.basename(three_d_model[0]) + "\n")
 
-    with open(point_cloud[0]) as three_d_model_file:
-        file.write(three_d_model_file.read())
+        with open(description[0]) as description_file:
+            file.write("// " + description_file.read() + "\n")
+
+        with open(point_cloud[0]) as three_d_model_file:
+            file.write(three_d_model_file.read())
+
+        for j in bar:
+            sleep(1)
+
+train()
