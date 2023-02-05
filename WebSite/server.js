6b93871e065363ddf4bf87c1e02848e1510fa517
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-05 16:18:16 +0100

    execute shell code

--- WebSite/server.js
+++ WebSite/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+var shell = require('shelljs');
+const { Triangle } = require('three');
 
 const app = express();
 const PORT = 9000;
@@ -14,6 +16,10 @@ app.get('/', function(req, res){
 app.get('/generate', function(req, res){
     res.render('generate');
 });
+
+async function execute(command){
+    await shell.exec(command);
+}
 
 app.listen(PORT, function(){
     console.log(`Server is running on port ${PORT}`);
