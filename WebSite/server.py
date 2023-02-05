f4a0a641257af9a89dafa1cc7b3f9c8ae8b41961
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-05 23:24:56 +0100

    Server + Train / Evaluate

--- WebSite/server.py
+++ WebSite/server.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 import sys
 
 sys.path.insert(1, '../AI/train')
+sys.path.insert(2, '../AI/')
 
-import evaluation
+from evaluation import *
+from get_prompt import *
 
 app = Flask(__name__)
 
@@ -12,9 +15,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/generate')
+@app.route('/generate', methods=['GET', 'POST'])
 def generate():
-    return render_template('generate.html')
+    if request.method == 'GET':
+        return render_template('generate.html')
+    elif request.method == 'POST':
+        return (f"Generating: {correct_prompt(request.form.get('prompt'))}")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9000)
