6bcbedb14e6b689d8cecb5b8f4355b2ab1bcbd50
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-07 13:56:40 +0100

    Connection with webserver

--- AI/Generate Model/model_generation.py
+++ AI/Generate Model/model_generation.py
@@ -1,27 +1,25 @@
 import numpy as np
 from point_cloud_to_mesh import *
 
-modelName = "../Train/model.ape"
+modelName = "../AI/Train/model.ape"
+iteration = 0
 
 def generate_model(prompt):
-    pass
+    print(prompt[0][0])
+    generate_single(prompt[0][0])
 
 def generate_single(word):
     foundModel = False
-    points = np.array([[0,0,0]])
-
     with open(modelName, "r") as file:
         while (line := file.readline()):
             if line[0] == "|" and foundModel:
-                point_cloud_to_mesh_obj(points, "model.obj")
+                print(np.loadtxt(f"generation {iteration}.txt"))
+                point_cloud_to_mesh_obj(np.loadtxt(f"generation {iteration}.txt"), "model.obj")
                 break
 
             elif foundModel:
-                print(line)
-                b = np.array([[float(line.split(' ')[0]), float(line.split(' ')[1]), float(line.split(' ')[2]) ]])
-                points = np.concatenate((points, b), axis=0)
+                with open(f"generation {iteration}.txt", "w") as f:
+                    f.write(line + "\n")
 
             elif word in line and not line[1] == "#":
                 foundModel = True
-
-generate_single("chair")
--- AI/Generate Model/point_cloud_to_mesh.py
+++ AI/Generate Model/point_cloud_to_mesh.py
@@ -3,8 +3,7 @@ from scipy.spatial import Delaunay
 import trimesh
 
 def point_cloud_to_mesh_obj(points, file_name):
-    tri = Delaunay(points)
-    faces = tri.simplices
-    mesh = trimesh.Trimesh(vertices=points, faces=faces)
-    mesh.export(file_name)
+    mesh = trimesh.PointCloud(points)
+    mesh = mesh.polygon_mesh(alpha=1.0)
+    mesh.export("mesh.ply")
 
