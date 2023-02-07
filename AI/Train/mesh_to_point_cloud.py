ce49a4cb2377dc8ee6ace724cfd536ca68f915c0
AlexSteiner30 <steiner.alex40@gmail.com>
2023-02-07 17:47:44 +0100

    Mesh a bit better

--- AI/Train/mesh_to_point_cloud.py
+++ AI/Train/mesh_to_point_cloud.py
@@ -5,7 +5,7 @@ def mesh_to_point_cloud(mesh_location, file_location):
     mesh = trimesh.load(mesh_location)
     vertices = mesh.vertices
 
-    step = int(vertices.shape[0]/10000)
+    step = int(vertices.shape[0]/30000)
     downsampled_vertices = vertices[::step, :]
 
     np.savetxt(file_location + "/point_cloud.pc", downsampled_vertices)
