153ba8f4fe1099d7daf9647e1b162f2ef7aeb479
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-07 19:15:32 +0100

    Stuff

--- AI/Generate Model/point_cloud_to_mesh.py
+++ AI/Generate Model/point_cloud_to_mesh.py
@@ -5,4 +5,4 @@ import trimesh
 def point_cloud_to_mesh_obj(points):
     point_cloud = pv.PolyData(points)
     mesh = point_cloud.reconstruct_surface()
-    mesh.plot(color='orange')
+    mesh.save('mesh.stl')
