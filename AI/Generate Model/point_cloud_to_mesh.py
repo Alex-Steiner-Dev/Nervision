ee43cdef1ec0cde268527e622287c049c5b356d0
AlexSteiner30 <steiner.alex40@gmail.com>
2023-02-09 20:48:45 +0100

    Save Model

--- AI/Generate Model/point_cloud_to_mesh.py
+++ AI/Generate Model/point_cloud_to_mesh.py
@@ -5,8 +5,7 @@ import pyvista as pv
 
 def point_cloud_to_mesh_obj(points):
     cloud = pv.PolyData(points)
-    cloud.plot()
 
     volume = cloud.delaunay_3d(alpha=2.)
     shell = volume.extract_geometry()
-    shell.plot()
+    shell.save('mesh.stl')
