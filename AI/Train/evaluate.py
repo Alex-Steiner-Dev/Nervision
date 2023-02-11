36a2c706321527968a1bd2517d8e1f9406b31bff
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-11 14:36:11 +0100

    Model Save + New Detection

--- AI/Train/evaluate.py
+++ AI/Train/evaluate.py
@@ -3,19 +3,17 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
-model = load_model("model.h5")
+# Load the saved model
+loaded_model = load_model("point_cloud_classifier.h5")
 
-def predict(input_points):
-    input_points = input_points[np.newaxis, ...]
-    predictions = model.predict(input_points)
-    return predictions.argmax(axis=-1)[0]
+# Preprocess a new point cloud
+new_point_cloud = trimesh.load("../Data/bathtub/test/bathtub_0141.off").sample(2048)
+new_point_cloud = new_point_cloud.reshape(1, -1) # Reshape the point cloud into a 2D array
 
-def detect(mesh_path):
-    mesh = trimesh.load(mesh_path)
-    points = mesh.sample(NUM_POINTS)
-    prediction = predict(points)
-    return prediction
 
-# Example usage
-detection = detect("../Data/bathtub/test/bathtub_0107.off")
-print("Detected class:", detection)
+predictions = loaded_model.predict(new_point_cloud)
+predicted_class = np.argmax(predictions)
+
+labels = {0: 'Data\\bathtub', 1: 'Data\\bed', 2: 'Data\\chair', 3: 'Data\\desk', 4: 'Data\\dresser', 5: 'Data\\monitor', 6: 'Data\\night_stand', 7: 'Data\\sofa', 8: 'Data\\table', 9: 'Data\\toilet'}
+
+print("Predicted class label:", labels[predicted_class])
--- AI/Train/train.py
+++ AI/Train/train.py
@@ -3,9 +3,10 @@ import glob
 import trimesh
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-from matplotlib import pyplot as plt
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.utils import to_categorical
 
 tf.random.set_seed(1234)
 
@@ -52,91 +53,27 @@ train_points, test_points, train_labels, test_labels, CLASS_MAP = parse_dataset(
     NUM_POINTS
 )
 
-def augment(points, label):
-    # jitter points
-    points += tf.random.uniform(points.shape, -0.005, 0.005, dtype=tf.float64)
-    # shuffle points
-    points = tf.random.shuffle(points)
-    return points, label
 
+print(CLASS_MAP)
 
-train_dataset = tf.data.Dataset.from_tensor_slices((train_points, train_labels))
-test_dataset = tf.data.Dataset.from_tensor_slices((test_points, test_labels))
+num_classes = len(np.unique(train_labels))
+train_points = train_points.reshape(train_points.shape[0], -1)
+test_points = test_points.reshape(test_points.shape[0], -1)
+train_labels = to_categorical(train_labels, num_classes)
+test_labels = to_categorical(test_labels, num_classes)
 
-train_dataset = train_dataset.shuffle(len(train_points)).map(augment).batch(BATCH_SIZE)
-test_dataset = test_dataset.shuffle(len(test_points)).batch(BATCH_SIZE)
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(train_points.shape[1],)))
+model.add(Dropout(0.5))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
 
-def conv_bn(x, filters):
-    x = layers.Conv1D(filters, kernel_size=1, padding="valid")(x)
-    x = layers.BatchNormalization(momentum=0.0)(x)
-    return layers.Activation("relu")(x)
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+model.fit(train_points, train_labels, epochs=500, batch_size=64, validation_data=(test_points, test_labels))
 
-def dense_bn(x, filters):
-    x = layers.Dense(filters)(x)
-    x = layers.BatchNormalization(momentum=0.0)(x)
-    return layers.Activation("relu")(x)
+test_loss, test_acc = model.evaluate(test_points, test_labels)
+print('Test accuracy:', test_acc)
 
-
-class OrthogonalRegularizer(keras.regularizers.Regularizer):
-    def __init__(self, num_features, l2reg=0.001):
-        self.num_features = num_features
-        self.l2reg = l2reg
-        self.eye = tf.eye(num_features)
-
-    def __call__(self, x):
-        x = tf.reshape(x, (-1, self.num_features, self.num_features))
-        xxt = tf.tensordot(x, x, axes=(2, 2))
-        xxt = tf.reshape(xxt, (-1, self.num_features, self.num_features))
-        return tf.reduce_sum(self.l2reg * tf.square(xxt - self.eye))
-
-def tnet(inputs, num_features):
-    bias = keras.initializers.Constant(np.eye(num_features).flatten())
-    reg = OrthogonalRegularizer(num_features)
-
-    x = conv_bn(inputs, 32)
-    x = conv_bn(x, 64)
-    x = conv_bn(x, 512)
-    x = layers.GlobalMaxPooling1D()(x)
-    x = dense_bn(x, 256)
-    x = dense_bn(x, 128)
-    x = layers.Dense(
-        num_features * num_features,
-        kernel_initializer="zeros",
-        bias_initializer=bias,
-        activity_regularizer=reg,
-    )(x)
-    feat_T = layers.Reshape((num_features, num_features))(x)
- 
-    return layers.Dot(axes=(2, 1))([inputs, feat_T])
-
-inputs = keras.Input(shape=(NUM_POINTS, 3))
-
-x = tnet(inputs, 3)
-x = conv_bn(x, 32)
-x = conv_bn(x, 32)
-x = tnet(x, 32)
-x = conv_bn(x, 32)
-x = conv_bn(x, 64)
-x = conv_bn(x, 512)
-x = layers.GlobalMaxPooling1D()(x)
-x = dense_bn(x, 256)
-x = layers.Dropout(0.3)(x)
-x = dense_bn(x, 128)
-x = layers.Dropout(0.3)(x)
-
-outputs = layers.Dense(NUM_CLASSES, activation="softmax")(x)
-
-model = keras.Model(inputs=inputs, outputs=outputs, name="pointnet")
-model.summary()
-
-
-model.compile(
-    loss="sparse_categorical_crossentropy",
-    optimizer=keras.optimizers.Adam(learning_rate=0.001),
-    metrics=["sparse_categorical_accuracy"],
-)
-
-model.fit(train_dataset,  epochs=1, validation_data=test_dataset)
-model.layers = [layer for layer in model.layers if layer.__class__.__name__ != "OrthogonalRegularizer"]
-model.save("model.h5")
+model.save("point_cloud_classifier.h5")
