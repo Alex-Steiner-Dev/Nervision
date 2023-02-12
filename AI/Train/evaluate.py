70a4a24e4953bd5fb08f352482d8fd9886bbee96
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-12 08:46:17 +0100

    Some Fixes

--- AI/Train/evaluate.py
+++ AI/Train/evaluate.py
@@ -1,64 +1,16 @@
-import os
-import glob
 import trimesh
 import numpy as np
 import tensorflow as tf
-from matplotlib import pyplot as plt
 from tensorflow.keras.models import load_model
 
 model = load_model("point_cloud_classifier.h5")
 
-DATA_DIR = "../Data/*"
 CLASS_MAP = {0: "Bathtub", 1:"Bed", 2:"Chair", 3:"Desk", 4:"Dresser", 5:"Monitor", 6:"Night Stand", 7:"Sofa", 8:"Table", 9:"Toilet"}
 
-def parse_dataset(num_points=2048):
-    test_points = []
-    test_labels = []
-
-    folders = glob.glob(DATA_DIR)
-
-    for i, folder in enumerate(folders):
-        print("processing class: {}".format(os.path.basename(folder)))
-
-        test_files = glob.glob(folder + "/test/*")
-
-        for f in test_files:
-            test_points.append(trimesh.load(f).sample(num_points))
-            test_labels.append(i)
-
-    return (
-        np.array(test_points),
-        np.array(test_labels),
-    )
-
-NUM_POINTS = 2048
-NUM_CLASSES = 10
-BATCH_SIZE = 32
-
-test_points, test_labels = parse_dataset(NUM_POINTS)
-
-test_dataset = tf.data.Dataset.from_tensor_slices((test_points, test_labels))
-test_dataset = test_dataset.shuffle(len(test_points)).batch(BATCH_SIZE)
-
-data = test_dataset.take(1)
-
-points, labels = list(data)[0]
-points = points[:8, ...]
-labels = labels[:8, ...]
+points = trimesh.load("../Data/desk/test/desk_0201.off").sample(2048)
+print(points.shape)
 
 preds = model.predict(points)
 preds = tf.math.argmax(preds, -1)
 
-points = points.numpy()
-
-fig = plt.figure(figsize=(15, 10))
-for i in range(8):
-    ax = fig.add_subplot(2, 4, i + 1, projection="3d")
-    ax.scatter(points[i, :, 0], points[i, :, 1], points[i, :, 2])
-    ax.set_title(
-        "pred: {:}, label: {:}".format(
-            CLASS_MAP[preds[i].numpy()], CLASS_MAP[labels.numpy()[i]]
-        )
-    )
-    ax.set_axis_off()
-plt.show()
+print("pred: {:}".format(CLASS_MAP[preds.numpy()]))
--- AI/Train/train.py
+++ AI/Train/train.py
@@ -11,19 +11,15 @@ tf.random.set_seed(1234)
 DATA_DIR = "../Data/*"
 
 def parse_dataset(num_points=2048):
-
     train_points = []
     train_labels = []
     test_points = []
     test_labels = []
-    class_map = {}
     folders = glob.glob(DATA_DIR)
 
     for i, folder in enumerate(folders):
-        print("processing class: {}".format(os.path.basename(folder)))
+        print("Processing class: {}".format(os.path.basename(folder)))
     
-        class_map[i] = folder.split("/")[-1]
-  
         train_files = glob.glob(folder + "/train/*")
         test_files = glob.glob(folder + "/test/*")
 
@@ -40,16 +36,13 @@ def parse_dataset(num_points=2048):
         np.array(test_points),
         np.array(train_labels),
         np.array(test_labels),
-        class_map,
     )
 
 NUM_POINTS = 2048
 NUM_CLASSES = 10
 BATCH_SIZE = 32
 
-train_points, test_points, train_labels, test_labels, CLASS_MAP = parse_dataset(
-    NUM_POINTS
-)
+train_points, test_points, train_labels, test_labels = parse_dataset()
 
 def augment(points, label):
     points += tf.random.uniform(points.shape, -0.005, 0.005, dtype=tf.float64)
@@ -93,7 +86,6 @@ def tnet(inputs, num_features):
   
     return layers.Dot(axes=(2, 1))([inputs, feat_T])
 
-
 inputs = keras.Input(shape=(NUM_POINTS, 3))
 
 x = tnet(inputs, 3)
@@ -111,7 +103,7 @@ x = layers.Dropout(0.3)(x)
 
 outputs = layers.Dense(NUM_CLASSES, activation="softmax")(x)
 
-model = keras.Model(inputs=inputs, outputs=outputs, name="pointnet")
+model = keras.Model(inputs=inputs, outputs=outputs, name="classification")
 model.summary()
 
 model.compile(
