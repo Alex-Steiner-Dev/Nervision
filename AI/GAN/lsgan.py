85512c8045b4c1365095dda4f212395391e14aef
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-15 16:03:37 +0100

    Stuff

--- AI/GAN/lsgan.py
+++ AI/GAN/lsgan.py
@@ -6,34 +6,34 @@ from tensorflow.keras.optimizers import Adam
 def build_generator(input_shape):
     x = Input(input_shape)
 
-    y = Conv1DTranspose(512, 3, strides=2, padding="same")(x)
+    y = Conv1DTranspose(512, 3, strides=1, padding="same")(x)
     y = LeakyReLU(0.2)(y)
 
-    y = Conv1DTranspose(256, 3, strides=2, padding="same")(y)
+    y = Conv1DTranspose(256, 3, strides=1, padding="same")(y)
     y = LeakyReLU(0.2)(y)
 
-    y = Conv1DTranspose(128, 3, strides=2, padding="same")(y)
+    y = Conv1DTranspose(128, 3, strides=1, padding="same")(y)
     y = LeakyReLU(0.2)(y)
 
-    y = Conv1DTranspose(64, 3, strides=2, padding="same")(y)
+    y = Conv1DTranspose(64, 3, strides=1, padding="same")(y)
     y = LeakyReLU(0.2)(y)
 
     y = Conv1D(3, 3, padding="same", activation="tanh")(y)
-    return Model(x, y)
+    return Model(x,y)
 
 def build_discriminator(input_shape):
     x = Input(input_shape)
 
-    y = Conv1D(64, 3, strides=2, padding="same")(x)
+    y = Conv1D(64, 3, strides=1, padding="same")(x)
     y = LeakyReLU(0.2)(y)
 
-    y = Conv1D(128, 3, strides=2, padding="same")(y)
+    y = Conv1D(128, 3, strides=1, padding="same")(y)
     y = LeakyReLU(0.2)(y)
 
-    y = Conv1D(256, 3, strides=2, padding="same")(y)
+    y = Conv1D(256, 3, strides=1, padding="same")(y)
     y = LeakyReLU(0.2)(y)
 
-    y = Conv1D(512, 3, strides=2, padding="same")(y)
+    y = Conv1D(512, 3, strides=1, padding="same")(y)
     y = LeakyReLU(0.2)(y)
 
     y = GlobalAveragePooling1D()(y)
@@ -52,8 +52,15 @@ def build_train_step(generator, discriminator):
         pred_real = discriminator(real_image)
         pred_fake = discriminator(fake_image)
 
-        LAMBA = 0.0002
-        d_loss = tf.reduce_mean(tf.maximum(pred_real - pred_fake + LAMBA * tf.reduce_sum(tf.abs(real_image-fake_image),axis=[1,2,3]), 0.0))
+        LAMBA = tf.cast(0.0002, dtype=tf.float32)
+
+        pred_real = tf.cast(pred_real, dtype=tf.float32)
+        pred_fake = tf.cast(pred_fake, dtype=tf.float32)
+
+        real_image = tf.cast(real_image, dtype=tf.float32)
+        fake_image = tf.cast(fake_image, dtype=tf.float32)
+
+        d_loss = tf.reduce_mean(tf.maximum(pred_real - pred_fake + LAMBA * tf.reduce_sum(tf.abs(real_image-fake_image)), 0.0))
         g_loss = tf.reduce_mean(pred_fake)
 
         d_gradients = tf.gradients(d_loss, discriminator.trainable_variables)
--- AI/GAN/train.py
+++ AI/GAN/train.py
@@ -12,18 +12,18 @@ noise_size = (2048,3)
 batch_size = 16
 
 x_train = parse_dataset()
-time.sleep(10)
-num_of_data = 100
 
 generator = build_generator(noise_size)
 discriminator = build_discriminator(model_size)
 train_step = build_train_step(generator, discriminator)
 
+generator.summary()
+discriminator.summary()
+
 for e in range(epoch):
     for s in range(steps):
         real_models = x_train[0]
         noise = np.random.rand(1, 2048, 3)
-
         d_loss, g_loss = train_step(real_models, noise)
         print ("[{0}/{1}] [{2}/{3}] d_loss: {4:.4}, g_loss: {5:.4}".format(e, epoch, s, steps, d_loss, g_loss))
 
