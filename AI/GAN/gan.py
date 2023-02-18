e92d6901b0ba75f704e5b41c9817220930f7933a
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-18 18:16:05 +0100

    Bug Fix

--- AI/GAN/gan.py
+++ AI/GAN/gan.py
@@ -24,31 +24,31 @@ def discriminator():
     output = Dense(1, activation="sigmoid")(x)
 
     model = Model(input, output)
-    model.compile(optimizer=Adam(lr=0.0002, beta_1=0.5), loss="binary_crossentropy")
+    model.compile(optimizer=Adam(learning_rate=0.0002, beta_1=0.5), loss="binary_crossentropy")
 
     return model
 
 def generator():
-    input = Input(shape=(2048,3))
+    input = Input(shape=(2048))
 
     x = Dense(256, activation=LeakyReLU(alpha=0.2))(input)
     x = Dense(512, activation=LeakyReLU(alpha=0.2))(input)
     x = Dense(1024, activation=LeakyReLU(alpha=0.2))(input)
     x = Dense(6144, activation=LeakyReLU(alpha=0.2))(input)
 
-    output = Reshape((2048,3))(x)
+    output = Reshape((2048, 3))(x)
 
     return Model(input, output)
 
 def gan(discr, gener):
     discr.trainable = False
 
-    model = Sequential({
+    model = Sequential([
         gener,
         discr
-    })
+    ])
 
-    model.compile(optimizer=Adam(lr=0.0002, beta_1=0.5), loss="binary_crossentropy")
+    model.compile(optimizer=Adam(learning_rate=0.0002, beta_1=0.5), loss="binary_crossentropy")
 
     return model
 
