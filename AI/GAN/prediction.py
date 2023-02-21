e5688695855eadea4f78ac38a8bdd26f675cf5f3
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-21 21:53:09 +0100

    Prediction

--- AI/GAN/prediction.py
+++ AI/GAN/prediction.py
@@ -1,9 +1,9 @@
+from models import Generator
 import numpy as np
 import matplotlib.pyplot as plt
-from gan import *
+import os
 
-batch_size = 1
-z_size = 200
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
 def saveFromVoxels(voxels, path):
     z, x, y = voxels.nonzero()
@@ -12,15 +12,12 @@ def saveFromVoxels(voxels, path):
     ax.scatter(x, y, -z, zdir='z', c='red')
     plt.savefig(path)
 
-generator = build_generator()
+def test():
+    generator = Generator().build_generator()
+    generator.load_weights('Models\generator_epoch_501.h5')
+    sample_noise = np.random.normal(0, 0.33, size=[1, 1, 1, 1, 200]).astype(np.float32)
+    generated_volumes = generator.predict(sample_noise, verbose=1)
+    generated_volumes = generated_volumes.reshape(64, 64,64)
+    saveFromVoxels(generated_volumes, "test.png")
 
-generator.load_weights("generator_weights.h5")
-
-z_sample = np.random.normal(0, 1, size=[batch_size, 1, 1, 1, z_size]).astype(np.float32)
-generated_volumes = generator.predict(z_sample, verbose=3)
-
-for i, generated_volume in enumerate(generated_volumes[:2]):
-            voxels = np.squeeze(generated_volume)
-            voxels[voxels < 0.5] = 0.
-            voxels[voxels >= 0.5] = 1.
-            saveFromVoxels(voxels, "gen_{}".format(i))
+test()
