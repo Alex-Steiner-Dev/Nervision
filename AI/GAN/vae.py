72787f6259152e8f5d7905c15faf69354d7b4257
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-22 16:39:37 +0100

    VAE Demo

--- AI/GAN/vae.py
+++ AI/GAN/vae.py
@@ -1,22 +1,19 @@
-import trimesh
 import numpy as np
-from keras.models import Model
-from keras.layers import Input, Conv3D, Flatten, Dense, Reshape, Conv3DTranspose, Lambda
-import keras.backend as K
 import keras
+import open3d
 import trimesh
-import open3d as o3d
 
 # Load the chair mesh using Trimesh
 chair_mesh = trimesh.load('../Data/chair/train/chair_0001.off')
+points = chair_mesh.sample(1000000)
 
-# Convert the mesh to a point cloud using PyVista
-point_cloud = chair_mesh.sample(10000)
 
-# Define the Keras VAE model
+points = (points - points.mean(axis=0)) / points.std(axis=0) # normalize the points
+
+# Define the encoder and decoder architectures
 latent_dim = 128
 
-encoder_inputs = keras.Input(shape=(point_cloud.shape[1],))
+encoder_inputs = keras.Input(shape=(points.shape[1],))
 x = keras.layers.Dense(256, activation='relu')(encoder_inputs)
 x = keras.layers.Dense(128, activation='relu')(x)
 z_mean = keras.layers.Dense(latent_dim)(x)
@@ -27,7 +24,7 @@ z = z_mean + keras.backend.exp(0.5 * z_log_var) * keras.backend.random_normal(sh
 decoder_inputs = keras.Input(shape=(latent_dim,))
 x = keras.layers.Dense(128, activation='relu')(decoder_inputs)
 x = keras.layers.Dense(256, activation='relu')(x)
-decoder_outputs = keras.layers.Dense(point_cloud.shape[1])(x)
+decoder_outputs = keras.layers.Dense(points.shape[1])(x)
 
 encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name='encoder')
 decoder = keras.Model(decoder_inputs, decoder_outputs, name='decoder')
@@ -35,19 +32,27 @@ decoder = keras.Model(decoder_inputs, decoder_outputs, name='decoder')
 vae_outputs = decoder(encoder(encoder_inputs)[2])
 vae = keras.Model(encoder_inputs, vae_outputs, name='vae')
 
-# Train the VAE on the point cloud
-vae.compile(optimizer=keras.optimizers.Adam(lr=0.001),
-            loss=keras.losses.mean_squared_error)
-vae.fit(point_cloud, point_cloud, epochs=50, batch_size=32)
+# Define the loss function
+reconstruction_loss = keras.losses.mean_squared_error(encoder_inputs, vae_outputs)
+reconstruction_loss *= points.shape[1]
+kl_loss = 1 + z_log_var - keras.backend.square(z_mean) - keras.backend.exp(z_log_var)
+kl_loss = keras.backend.sum(kl_loss, axis=-1)
+kl_loss *= -0.5
+vae_loss = keras.backend.mean(reconstruction_loss + kl_loss)
+vae.add_loss(vae_loss)
 
-# Generate a new set of latent vectors
-latent_vectors = np.random.normal(size=(1, latent_dim))
+# Train the VAE
+vae.compile(optimizer=keras.optimizers.Adam(lr=0.001))
+vae.fit(points, epochs=10000, batch_size=1)
 
-# Use the VAE decoder to generate a new set of points
+vae.save("vae.h5")
+
+# Generate new point clouds from the VAE
+latent_vectors = np.random.normal(size=(16384, latent_dim))
 generated_points = decoder.predict(latent_vectors)
 
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(point_cloud)
+pcd = open3d.geometry.PointCloud()
+pcd.points = open3d.utility.Vector3dVector(generated_points)
 
 # Visualize the point cloud
-o3d.visualization.draw_geometries([pcd])
+open3d.visualization.draw_geometries([pcd])
