2820052d0b8bdf75bcf9db5182cf33c72b83b951
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-23 21:10:30 +0100

    Model

--- AI/GAN/vae.py
+++ AI/GAN/vae.py
@@ -1,62 +1,63 @@
 import numpy as np
-from keras.layers import Input, Dense, Lambda, Reshape, Flatten
-from keras.models import Model
-from keras import backend as K
-import trimesh
 import keras
+import open3d
+import trimesh
 
-chair_meshes = trimesh.load("../Data/chair/train/chair_0001.off")
-chair_point_clouds = chair_meshes.sample(2048)
-chair_point_clouds = chair_point_clouds.reshape(1, 2048, 3)
+# Load the chair mesh using Trimesh
+chair_mesh = trimesh.load('../Data/monitor/train/monitor_0001.off')
+points = chair_mesh.sample(4096)
 
-latent_dim = 2
+points = (points - points.mean(axis=0)) / points.std(axis=0) # normalize the points
 
-def sampling(args):
-    """Reparameterization trick to sample from the latent space"""
-    z_mean, z_log_var = args
-    epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim), mean=0., stddev=1.)
-    return z_mean + K.exp(z_log_var / 2) * epsilon
+# Define the encoder and decoder architectures
+latent_dim = 128
 
-input_shape = (2048, 3)
-inputs = Input(shape=input_shape, name='encoder_input')
-x = Flatten()(inputs)
-x = Dense(512, activation='relu')(x)
-x = Dense(256, activation='relu')(x)
-z_mean = Dense(latent_dim, name='z_mean')(x)
-z_log_var = Dense(latent_dim, name='z_log_var')(x)
+encoder_inputs = keras.Input(shape=(points.shape[1],))
+x = keras.layers.Dense(256, activation='relu')(encoder_inputs)
+x = keras.layers.Dense(128, activation='relu')(x)
+z_mean = keras.layers.Dense(latent_dim)(x)
+z_log_var = keras.layers.Dense(latent_dim)(x)
 
-z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
+z = z_mean + keras.backend.exp(0.5 * z_log_var) * keras.backend.random_normal(shape=(latent_dim,))
 
-encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
+decoder_inputs = keras.Input(shape=(latent_dim,))
+x = keras.layers.Dense(128, activation='relu')(decoder_inputs)
+x = keras.layers.Dense(256, activation='relu')(x)
+decoder_outputs = keras.layers.Dense(points.shape[1])(x)
 
-latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
-x = Dense(256, activation='relu')(latent_inputs)
-x = Dense(512, activation='relu')(x)
-x = Dense(2048*3, activation='sigmoid')(x)
-outputs = Reshape((2048, 3))(x)
+encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name='encoder')
+decoder = keras.Model(decoder_inputs, decoder_outputs, name='decoder')
 
-decoder = Model(latent_inputs, outputs, name='decoder')
+vae_outputs = decoder(encoder(encoder_inputs)[2])
+vae = keras.Model(encoder_inputs, vae_outputs, name='vae')
 
-outputs = decoder(encoder(inputs)[2])
-vae = Model(inputs, outputs, name='vae')
-
-reconstruction_loss = keras.losses.binary_crossentropy(Flatten()(inputs), Flatten()(outputs))
-reconstruction_loss *= 2048*3
-kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
-kl_loss = K.sum(kl_loss, axis=-1)
+# Define the loss function
+reconstruction_loss = keras.losses.mean_squared_error(encoder_inputs, vae_outputs)
+reconstruction_loss *= points.shape[1]
+kl_loss = 1 + z_log_var - keras.backend.square(z_mean) - keras.backend.exp(z_log_var)
+kl_loss = keras.backend.sum(kl_loss, axis=-1)
 kl_loss *= -0.5
-vae_loss = K.mean(reconstruction_loss + kl_loss)
+vae_loss = keras.backend.mean(reconstruction_loss + kl_loss)
 vae.add_loss(vae_loss)
-vae.compile(optimizer='adam')
 
-vae.fit(chair_point_clouds, epochs=50, batch_size=1)
+# Train the VAE
+vae.compile(optimizer=keras.optimizers.Adam(lr=0.001))
+vae.fit(points, epochs=1000, batch_size=1)
 
-# Generate new point clouds from the trained VAE model
-z_sample = np.random.normal(size=(2048, latent_dim))
-x_decoded = decoder.predict(z_sample)
+vae.save_weights("vae.h5")
 
-mesh = trimesh.Trimesh(vertices=x_decoded[0], faces=None)
-mesh = mesh.fix_normals()
+# Generate new point clouds from the VAE
+latent_vectors = np.random.normal(size=(4096, latent_dim))
+generated_points = decoder.predict(latent_vectors)
+
+pcd = open3d.geometry.PointCloud()
+pcd.points = open3d.utility.Vector3dVector(generated_points)
+
+# Visualize the point cloud
+open3d.visualization.draw_geometries([pcd])
+
+#mesh = trimesh.Trimesh(vertices=x_decoded[0], faces=None)
+#mesh = mesh.fix_normals()
 #mesh = mesh.fill_holes()
 #mesh = mesh.compute_convex_hull()
 #mesh = mesh.split()[0]
