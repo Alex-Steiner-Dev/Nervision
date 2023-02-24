ecda6301f1f0e54ba1e677a9c91a4737869ff81b
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-24 17:23:14 +0100

    Stuff

--- AI/GAN/dataset.py
+++ AI/GAN/dataset.py
@@ -14,7 +14,19 @@ def parse_dataset():
     for i, folder in enumerate(folders):
         train_files = glob.glob(folder + "/train/*.off")
 
-        point_cloud = trimesh.load(train_files[0]).sample(4096)
-        objects.append(point_cloud)
+        point_cloud = trimesh.load(train_files[0]).sample(32)
+
+        x, y, z = np.meshgrid(np.linspace(0, 1, 32),
+                      np.linspace(0, 1, 32),
+                      np.linspace(0, 1, 32))
+        grid = np.stack([x, y, z], axis=-1)
+
+        # Compute the Euclidean distances between each point and each voxel
+        distances = np.linalg.norm(point_cloud[:, None, None, :] - grid[None, :, :, :], axis=-1)
+
+        # Assign each point to the closest voxel
+        voxels = np.argmin(distances, axis=1)
+
+        objects.append(voxels)
 
     return objects
