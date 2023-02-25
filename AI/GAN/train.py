6667686a06b30603974f79db0d99de929c53554f
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-25 12:40:28 +0100

    Airplane generation

--- AI/GAN/train.py
+++ AI/GAN/train.py
@@ -8,7 +8,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from keras import backend as K
 K.clear_session()
 
-train_data = parse_dataset()[0]
+train_data = parse_dataset()[7]
 
 resolutions = []
 box_size = 32
@@ -17,6 +17,6 @@ train_data = train_data.reshape([-1, box_size, box_size, box_size, 1])
 
 autoencoder = VAE(box_size=box_size, resolutions=resolutions).build_vae()
 
-history = autoencoder.fit(train_data, train_data, epochs=5000, batch_size=1)
+history = autoencoder.fit(train_data, train_data, epochs=50000, batch_size=1)
 
 autoencoder.save('autoencoder.h5')
