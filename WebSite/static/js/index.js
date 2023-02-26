560ff5f6315919adc9386513255fc5c8ebda8bd6
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-02-26 22:16:01 +0100

    Load Function + Drop Down

--- WebSite/static/js/index.js
+++ WebSite/static/js/index.js
@@ -1,3 +1,4 @@
+// Initialize
 const canvas = document.getElementById('model-canvas');
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 
@@ -5,33 +6,61 @@ const fov = 45;
 const aspect = canvas.width / canvas.height;
 const near = 0.1;
 const far = 100;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.set(0, 0, 15); 
-camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-const controls = new THREE.OrbitControls(camera, canvas);
+document.getElementById("dropdown").style.visibility = "hidden";
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color('#111');
+// Load Model
+function loadModel(modelUrl){
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
-const ambientLight = new THREE.AmbientLight('#fff', 1);
-scene.add(ambientLight);
+  camera.position.set(0, 0, 15); 
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-const loader = new THREE.GLTFLoader();
-const modelUrl = '/static/chair.glb';
+  const controls = new THREE.OrbitControls(camera, canvas);
 
-loader.load(modelUrl, (gltf) => {
-  const model = gltf.scene;
-  model.position.set(0, 2, 0)
-  scene.add(model);
-});
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color('#111');
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio * 2); 
+  const ambientLight = new THREE.AmbientLight('#fff', 1);
+  scene.add(ambientLight);
 
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-  controls.update();
+  const loader = new THREE.GLTFLoader();
+
+  loader.load(modelUrl, (gltf) => {
+    const model = gltf.scene;
+    model.position.set(0, 2, 0)
+    scene.add(model);
+  });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio * 2); 
+
+  function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    controls.update();
+  }
+
+  animate();
+  dropdown();
 }
-animate();
+
+function dropdown(){
+  document.getElementById("dropdown").style.visibility = "visible"
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+
+        if (openDropdown.style.display === "block") {
+          openDropdown.style.display = "none";
+        }
+      }
+    }
+  }
+}
+
+loadModel("/static/chair.glb")
