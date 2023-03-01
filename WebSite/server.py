bee18c06f6a6995bef6fc0c84c959913251073da
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-03-01 14:09:37 +0100

    Login + Signup HTML & CSS

--- WebSite/server.py
+++ WebSite/server.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
