ef0e03061d90b49a05b482013e31cefbc835f080
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-03-08 12:36:53 +0100

    VAE

---
+++ AI/VAE/VAE.py
@@ -0,0 +1,26 @@
+from keras.layers import Input
+from keras.models import Model
+from keras.layers.convolutional import Convolution3D, MaxPooling3D, UpSampling3D
+
+class VAE:
+    def __init__(self, box_size):
+        self.box_size = box_size
+
+    def build_vae(self):
+        input_img = Input(shape=(self.box_size, self.box_size, self.box_size, 1))
+        x = Convolution3D(30, (5, 5, 5), activation='relu', padding='same')(input_img)
+        x = MaxPooling3D((2, 2, 2), padding='same')(x)
+        x = Convolution3D(60, (5, 5, 5), activation='relu', padding='same')(x)
+        x = Convolution3D(120, (5, 5, 5), activation='relu', padding='same')(x)
+        encoded = MaxPooling3D((2, 2, 2), padding='same', name='encoder')(x)
+
+        x = Convolution3D(60, (5, 5, 5), activation='relu', padding='same')(encoded)
+        x = UpSampling3D((2, 2, 2))(x)
+        x = Convolution3D(30, (5, 5, 5), activation='relu', padding='same')(x)
+        x = UpSampling3D((2, 2, 2))(x)
+        decoded = Convolution3D(1, (5, 5, 5), activation='relu', padding='same')(x)
+
+        autoencoder = Model(input_img, decoded)
+        autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+        return (autoencoder)
---
+++ AI/VAE/train.py
@@ -0,0 +1,24 @@
+from VAE.VAE import VAE
+from dataset import parse_dataset
+import numpy as np
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+from keras import backend as K
+K.clear_session()
+
+x_train, y_train = parse_dataset()
+x_train = np.array(x_train)
+
+box_size = 32
+
+print("Training...")
+
+autoencoder = VAE(box_size=box_size).build_vae()
+
+history = autoencoder.fit(x_train, x_train, epochs=1000, batch_size=100)
+
+autoencoder.save('../TrainedModels/autoencoder.h5')
+
+print("Done!")
