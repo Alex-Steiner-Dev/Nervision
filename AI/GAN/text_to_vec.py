bb9d5109fef40d6eefb453829a3480d65c2a784b
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-03-17 18:24:27 +0100

    Revert "Text To Vec"
    This reverts commit cf988f1b84255e50d5add46765735f39df4ecf23.

--- AI/GAN/text_to_vec.py
+++
@@ -1,7 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer('bert-base-nli-mean-tokens')
-
-def text_to_vec(input_txt):
-    input_vec = model.encode(input_txt)
-    return(input_vec)
