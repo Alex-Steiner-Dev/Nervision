9102595653c5c65004399334d63db82ee881c8f3
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-03-18 15:42:57 +0100

    Update mesh_generation.py

--- AI/GAN/mesh_generation.py
+++ AI/GAN/mesh_generation.py
@@ -19,29 +19,13 @@ def generate_mesh(points):
     )
     mesh = mesh.simplify_quadric_decimation(target_number_of_triangles=5000)
 
-    pcd = mesh.sample_points_uniformly(number_of_points=4096*2)
-    
-    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(
-        pcd, alpha=0.1
-    )
-
-    pcd = mesh.sample_points_uniformly(number_of_points=4096*4)
-    
-    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(
-        pcd, alpha=0.1
-    )
-
-    pcd = mesh.sample_points_uniformly(number_of_points=4096*6)
-    
-    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(
-        pcd, alpha=0.1
-    )
-
-    pcd = mesh.sample_points_uniformly(number_of_points=4096*8)
-    
-    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(
-        pcd, alpha=0.1
-    )
+    for i in range(9):
+        if not i == 0 and i % 2 == 0:
+            pcd = mesh.sample_points_uniformly(number_of_points=4096*i)
+            
+            mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(
+                pcd, alpha=0.1
+            )
 
     mesh.remove_duplicated_vertices()
     mesh.remove_degenerate_triangles()
