a196c46691cd033f0121d64b200a91d15eaacb96
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-03-19 14:36:03 +0100

    Text to 3d model

--- AI/GAN/predict.py
+++ AI/GAN/predict.py
@@ -3,16 +3,17 @@ from mesh_generation import generate_mesh
 from model import Generator
 import open3d as o3d
 from text_to_vec import *
+import pyvista as pv
 
 Generator = Generator(num_points=2048).cuda()
 
 model_path = "../TrainedModels/chair.pt" 
-model_path = "50.pt" 
+model_path = "900.pt" 
 checkpoint = torch.load(model_path)
 Generator.load_state_dict(checkpoint['G_state_dict'])
 
 #z = torch.randn(1, 1, 128).cuda()
-z = torch.from_numpy(text_to_vec("an american school bus with an open door")).reshape(1,1,128).cuda()
+z = torch.from_numpy(text_to_vec("long bus")).reshape(1,1,128).cuda()
 
 with torch.no_grad():
     sample = Generator(z).cpu()
@@ -22,5 +23,10 @@ with torch.no_grad():
     mesh = generate_mesh(points)
 
     o3d.io.write_triangle_mesh("generation.obj", mesh)
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
+
+    o3d.visualization.draw_geometries([pcd])
 
     o3d.visualization.draw_geometries([mesh])
