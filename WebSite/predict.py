45c51b863812abc617880dc1e3d5969d9db63f32
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-03-23 23:21:01 +0100

    Prediction Fix

--- WebSite/predict.py
+++ WebSite/predict.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append("../AI/GAN/")
 
 from mesh_generation import generate_mesh
-from text_to_vec import text_to_vec
+from text_to_vec import *
 from model import Generator
 import open3d as o3d
 import pyvista as pv
@@ -12,12 +12,12 @@ import numpy as np
 
 Generator = Generator(num_points=2048).cuda()
 
-model_path = "../AI/TrainedModels/1800.pt" 
+model_path = "../AI/TrainedModels/150.pt" 
 checkpoint = torch.load(model_path)
 Generator.load_state_dict(checkpoint['G_state_dict'])
 
 def generate(text):
-    z = torch.from_numpy(text_to_vec(text)).reshape(1,1,128).cuda()
+    z = torch.from_numpy(text_to_vec(process_text(text))).reshape(1,1,128).cuda()
 
     with torch.no_grad():
         sample = Generator(z).cpu()
