5c61c7460579e4ef74f373333585d95dd7d04aab
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-04-09 09:37:39 +0200

    Random Noise

--- AI/GAN/predict.py
+++ AI/GAN/predict.py
@@ -4,9 +4,8 @@ from model import Generator
 import open3d as o3d
 from text_to_vec import *
 import time
-import logging
-logger = logging.getLogger("trimesh")
-logger.setLevel(logging.ERROR)
+
+import numpy as np
 
 Generator = Generator(num_points=2048).cuda()
 
@@ -15,11 +14,12 @@ model_path = "300.pt"
 checkpoint = torch.load(model_path)
 Generator.load_state_dict(checkpoint['G_state_dict'])
 
-z = torch.from_numpy(text_to_vec(process_text(correct_prompt("a bathtub bowl")))).reshape(1,1,128).cuda()
-
+z = torch.from_numpy(text_to_vec(process_text(correct_prompt("bench"))) + np.random.normal(0, 0.01, 128).astype(np.float64)).reshape(1,1,128).cuda().float()
+print(z.type())
 
 with torch.no_grad():
     start = time.time()
+
     sample = Generator(z).cpu()
 
     points = sample.numpy().reshape(2048,3)
@@ -31,5 +31,7 @@ with torch.no_grad():
 
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(points)
+    
+    o3d.io.write_triangle_mesh("mesh.obj", mesh)
 
     o3d.visualization.draw_geometries([pcd])
