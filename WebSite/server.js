e8271a481aa070ae153dd91122fec286c7f4b073
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-04-14 11:47:20 +0200

    Implementation

--- WebSite/server.js
+++ WebSite/server.js
@@ -1,31 +1,33 @@
 const express = require('express');
 
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+
+require('dotenv').config()
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true})); 
 
 app.use(cookieParser());
-app.use(session({secret: "ghfdjkgdhkqEWQE9Qjkl329490ASH<A!àfsdfkJjgxDIHk284329FHDS_DASDJW_Dsdfdmsa"}));
+app.use(session({secret: process.env.SECRET}));
 
 app.set('view engine', 'ejs'); 
 app.use(express.static(__dirname + '/static'));
 
 const userSchema ={
-    userName: String,
+    mail: String,
     psw: String
 }
   
-const users = mongoose.model("users", userSchema)
+const users = mongoose.model('users', userSchema)
 
 app.get('/',async function(req, res){
-    if(req.session.username != null){  
-        users.find({ userName: req.session.username, psw:req.session.psw}).then( function(x){
+    if(req.session.mail != null){  
+        users.find({ mail: req.session.mail, psw:req.session.psw}).then( function(x){
             res.render('index-logged');
         });
     }
@@ -40,19 +42,18 @@ app.get('/login', function(req, res){
 });
 
 app.post('/login', async function(req, res){
-    const username = req.body.username;
+    const mail = req.body.mail;
     const psw = req.body.psw;
 
-    users.find({ userName: username, psw:psw}).then(function(x){
+    users.find({ mail: mail, psw:psw}).then(function(x){
         if(x.length != 0){
-            req.session.username = username;
+            req.session.mail = mail;
             req.session.psw = psw;
-            req.session.user = true;
 
             res.render('index-logged');
         }
         else{
-            res.send("Wrong data");
+            res.send('Wrong data');
         }
     });
 
@@ -63,11 +64,11 @@ app.get('/signup', function(req, res){
 });
 
 app.post('/signup', function(req, res){
-    const name = req.body.username;
+    const name = req.body.mail;
     const psw = req.body.psw;
 
     const newUser = new users({
-        userName: name,
+        mail: name,
         psw: psw
     })
 
@@ -76,13 +77,30 @@ app.post('/signup', function(req, res){
     res.render('login');
 });
 
+
+app.get('/logout', function(req, res){
+    req.session.mail = null;
+    req.session.password = null;
+
+    res.render('index');
+});
+
+app.get('/generation', function(req, res){
+    if(req.session.mail != null){
+        res.render('generation');
+    }
+    else{
+        res.render('index')
+    }
+});
+
 app.get('/download', function(req, res){
-    var path = "static/generation.glb";
+    var path = 'static/generation.glb';
 });
 
 app.listen(port=8080, function(){
-    console.log("Server is running on port 8080");
+    console.log('Server is running on port 8080');
 
-    mongoose.connect("mongodb+srv://francesco:x3UWKnnEYqVrcQNy@cluster.mpqus65.mongodb.net/")
-    console.log("Connect to the DB!");
+    mongoose.connect(process.env.MONGO_URI);
+    console.log('Connect to the DB!');
 });
