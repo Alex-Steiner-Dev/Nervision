9e585b178036e7a976141ae4db281d550e0f995e
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-05-08 18:17:10 +0200

    Updates

--- AI/GAN/predict.py
+++ AI/GAN/predict.py
@@ -13,11 +13,13 @@ model_path = "50.pt"
 checkpoint = torch.load(model_path)
 Generator.load_state_dict(checkpoint['G_state_dict'])
 
-z = torch.from_numpy(text_to_vec(process_text(correct_prompt("clock that is average thickness and average size"))).astype(np.float64)).reshape(1,512, 1).repeat(16, 1, 1).cuda().float()
+z = torch.from_numpy(text_to_vec(process_text(correct_prompt("bowl"))).astype(np.float64)).reshape(1,512, 1).repeat(16, 1, 1).cuda().float()
 
 with torch.no_grad():
     sample = Generator(z).cpu()
 
-    points = sample.numpy()[0]
+    vertices = sample.numpy()[0]
 
-    cloud = pv.PolyData(points).plot()
+    mesh = pv.PolyData(vertices)
+
+    mesh.plot()
