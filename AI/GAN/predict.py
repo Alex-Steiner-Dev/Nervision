8022a474c0fbc71ce2d0b64081ef967736e273b4
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-05-26 18:37:14 +0200

    Updates

--- AI/GAN/predict.py
+++ AI/GAN/predict.py
@@ -6,21 +6,18 @@ from mesh_generation import *
 import numpy as np
 import pyvista as pv
 
-from scipy.ndimage import median_filter
-
 Generator = Generator().cuda()
 
 model_path = "../TrainedModels/model.pt" 
 checkpoint = torch.load(model_path)
 Generator.load_state_dict(checkpoint['G_state_dict'])
 
-z = torch.from_numpy(text_to_vec(process_text(correct_prompt("straight chair that is large and short and wide and long"))).astype(np.float64)).reshape(1,512, 1).repeat(32,1,1).cuda().float()
+z = torch.from_numpy(text_to_vec(process_text(correct_prompt("cocktail table that is tall and square and average size"))).astype(np.float64)).reshape(1,512, 1).repeat(32,1,1).cuda().float()
 
 with torch.no_grad():
     sample = Generator(z).cpu()
 
     vertices = sample.numpy()[0]
-    vertices = median_filter(vertices, size=1)
 
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(vertices)
@@ -28,8 +25,9 @@ with torch.no_grad():
     cl, ind = pcd.remove_statistical_outlier(nb_neighbors=40,std_ratio=4.0)
     pcd = cl
 
-    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, alpha=0.1)
+    #mesh = generate_mesh(pcd.points)
 
-    mesh = mesh.filter_smooth_simple(number_of_iterations=1)
+    #o3d.visualization.draw_geometries([mesh])
 
-    o3d.visualization.draw_geometries([pcd, mesh])
+
+    pv.PolyData(vertices).plot()
